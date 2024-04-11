34c14c1ca2e7aca19b4c402f8b7d8fe2f784018c
leticia <leticia.nuness.santos@gmail.com>
2024-04-11 16:50:07 +0100

    commented out sql and took out method from route for now

--- application/data_access.py
+++ application/data_access.py
@@ -1,19 +1,19 @@
-import mysql.connector
-from datetime import datetime
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  # password="",
-  database=""
-)
-
-
-def get_db_connection():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        # password="password",
-        database=""
-    )
-    return mydb
+# import mysql.connector
+# from datetime import datetime
+#
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   # password="",
+#   database=""
+# )
+#
+#
+# def get_db_connection():
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         # password="password",
+#         database=""
+#     )
+#     return mydb
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from flask import url_for, render_template, request, redirect, session
 from datetime import datetime, timedelta
-from application.data_access import get_db_connection
+
 
 from application import app
 
